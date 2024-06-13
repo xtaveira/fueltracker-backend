c0d0f6674ba6d05e1e9a9660adc42a0073cab4fd
Davi Taveira <devtaveira@gmail.com>
2024-06-12 22:08:13 -0300

    updates to fix the .env

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3001/auth/google/callback', // Atualizado para a porta do backend
+      callbackURL: process.env.CALLBACK_URL, // Atualizado para a porta do backend
       scope: ['email', 'profile'],
     });
   }
